1fa7d5a9f9352bcef40ce9aee30596b8e40c03bd
Sadjad Fouladi <sfouladi@gmail.com>
2019-10-10 19:19:24 -0700

    Initial commit.

---
+++ js/draw.js
@@ -0,0 +1,51 @@
+treelets = [{"id": 0, "size": 342, "bounds": [[-0.99999994, -0.99999994, -0.99999994], [0.99999994, 0.99999994, 0.99999994]], "rays": 0, "parent": null, "children": [1, 2, 3, 4, 5, 6, 7, 8], "instances": [], "instanced_in": [], "main": true}, {"id": 1, "size": 21845620, "bounds": [[-0.0025843028, -0.99999994, -0.99999994], [0.99999994, 0.0025843028, 0.0]], "rays": 0, "parent": 0, "children": [11, 12], "instances": [], "instanced_in": [], "main": true}, {"id": 2, "size": 21532272, "bounds": [[-0.99999994, -0.99999994, -0.99999994], [0.0025843028, 0.0025843028, 0.0025843028]], "rays": 0, "parent": 0, "children": [9, 10], "instances": [], "instanced_in": [], "main": true}, {"id": 3, "size": 21845370, "bounds": [[-0.0025843028, 0.0, -0.99999994], [0.99999994, 0.99999994, 0.0025843028]], "rays": 0, "parent": 0, "children": [15, 16], "instances": [], "instanced_in": [], "main": true}, {"id": 4, "size": 21845839, "bounds": [[-0.99999994, -0.00258087763, -0.99999994], [0.0025843028, 0.99999994, 0.0025843028]], "rays": 0, "parent": 0, "children": [13, 14], "instances": [], "instanced_in": [], "main": true}, {"id": 5, "size": 21844079, "bounds": [[-0.0025843028, -0.99999994, -0.0025843028], [0.99999994, 0.00254437514, 0.99999994]], "rays": 0, "parent": 0, "children": [19, 20], "instances": [], "instanced_in": [], "main": true}, {"id": 6, "size": 21844216, "bounds": [[-0.99999994, -0.99999994, -0.0025843028], [0.0025843028, 0.0, 0.99999994]], "rays": 0, "parent": 0, "children": [17, 18], "instances": [], "instanced_in": [], "main": true}, {"id": 7, "size": 21845708, "bounds": [[-0.00258430233, -0.0025843028, -0.0025443749], [0.99999994, 0.99999994, 0.99999994]], "rays": 0, "parent": 0, "children": [23, 24], "instances": [], "instanced_in": [], "main": true}, {"id": 8, "size": 21846241, "bounds": [[-0.99999994, -0.0025843028, 0.0], [0.0025843028, 0.99999994, 0.99999994]], "rays": 0, "parent": 0, "children": [21, 22], "instances": [], "instanced_in": [], "main": true}, {"id": 9, "size": 696010, "bounds": [[-0.18240054, -0.263761997, -0.99999994], [0.0, 0.00258089043, -0.94846189]], "rays": 0, "parent": 2, "children": [], "instances": [], "instanced_in": [], "main": true}, {"id": 10, "size": 350645, "bounds": [[-0.99999994, -0.178267971, -0.142919093], [-0.973903537, 0.0, 0.0]], "rays": 0, "parent": 2, "children": [], "instances": [], "instanced_in": [], "main": true}, {"id": 11, "size": 696028, "bounds": [[0.0, -0.269958794, -0.99999994], [0.178259239, 0.00258089043, -0.947520614]], "rays": 0, "parent": 1, "children": [], "instances": [], "instanced_in": [], "main": true}, {"id": 12, "size": 350324, "bounds": [[0.972225487, -0.124845602, -0.198345274], [0.99999994, 0.0, 0.0]], "rays": 0, "parent": 1, "children": [], "instances": [], "instanced_in": [], "main": true}, {"id": 13, "size": 695933, "bounds": [[-0.182442203, 0.0, -0.99999994], [0.0, 0.265629441, -0.94846189]], "rays": 0, "parent": 4, "children": [], "instances": [], "instanced_in": [], "main": true}, {"id": 14, "size": 350649, "bounds": [[-0.99999994, 0.0, -0.140754923], [-0.975001454, 0.178260803, 0.00258089043]], "rays": 0, "parent": 4, "children": [], "instances": [], "instanced_in": [], "main": true}, {"id": 15, "size": 696049, "bounds": [[0.0, 0.0, -0.99999994], [0.182442203, 0.265629441, -0.94846189]], "rays": 0, "parent": 3, "children": [], "instances": [], "instanced_in": [], "main": true}, {"id": 16, "size": 350612, "bounds": [[0.975001454, 0.0, -0.140754923], [0.99999994, 0.178260803, 0.00258089043]], "rays": 0, "parent": 3, "children": [], "instances": [], "instanced_in": [], "main": true}, {"id": 17, "size": 350516, "bounds": [[-0.99999994, -0.178260803, -0.00258089043], [-0.975001454, 0.0, 0.140754923]], "rays": 0, "parent": 6, "children": [], "instances": [], "instanced_in": [], "main": true}, {"id": 18, "size": 695813, "bounds": [[-0.182442203, -0.265629441, 0.94846189], [0.0, 0.0, 0.99999994]], "rays": 0, "parent": 6, "children": [], "instances": [], "instanced_in": [], "main": true}, {"id": 19, "size": 350511, "bounds": [[0.974518478, -0.178266138, -0.00258089043], [0.99999994, 0.0, 0.140751272]], "rays": 0, "parent": 5, "children": [], "instances": [], "instanced_in": [], "main": true}, {"id": 20, "size": 695846, "bounds": [[0.0, -0.265610069, 0.94846189], [0.182447746, 0.0, 0.99999994]], "rays": 0, "parent": 5, "children": [], "instances": [], "instanced_in": [], "main": true}, {"id": 21, "size": 350426, "bounds": [[-0.99999994, 0.0, 0.0], [-0.972225487, 0.124845602, 0.198345274]], "rays": 0, "parent": 8, "children": [], "instances": [], "instanced_in": [], "main": true}, {"id": 22, "size": 695973, "bounds": [[-0.178259239, -0.00258089043, 0.947520614], [0.0, 0.269958794, 0.99999994]], "rays": 0, "parent": 8, "children": [], "instances": [], "instanced_in": [], "main": true}, {"id": 23, "size": 350768, "bounds": [[0.974378943, 0.0, 0.0], [0.99999994, 0.178266138, 0.143106699]], "rays": 0, "parent": 7, "children": [], "instances": [], "instanced_in": [], "main": true}, {"id": 24, "size": 696048, "bounds": [[0.0, -0.00258089043, 0.94846189], [0.182349652, 0.263765067, 0.99999994]], "rays": 0, "parent": 7, "children": [], "instances": [], "instanced_in": [], "main": true}];
+
+window.addEventListener("DOMContentLoaded", function() {
+  var scene = new THREE.Scene();
+  var camera = new THREE.PerspectiveCamera(
+      75, window.innerWidth / window.innerHeight, 0.1, 10000);
+
+  scene.background = new THREE.Color(0xf0f0f0);
+
+  var light = new THREE.AmbientLight( 0x404040 );
+  //light.position.set(0, 0, 0).normalize();
+  scene.add(light);
+
+  /* drawing cubes */
+  var geometry = new THREE.BoxBufferGeometry(1, 1, 1);
+
+  for (var id in treelets) {
+    let treelet = treelets[id];
+
+    var object = new THREE.Mesh(
+        geometry, new THREE.MeshLambertMaterial(
+                      {color : Math.random() * 0xffffff, opacity : 0.5, transparent: true}));
+
+    object.position.x = (treelet.bounds[0][0] + treelet.bounds[1][0]) / 2;
+    object.position.y = (treelet.bounds[0][1] + treelet.bounds[1][1]) / 2;
+    object.position.z = (treelet.bounds[0][2] + treelet.bounds[1][2]) / 2;
+
+    object.scale.x = (treelet.bounds[1][0] - treelet.bounds[0][0]) * 0.95;
+    object.scale.y = (treelet.bounds[1][1] - treelet.bounds[0][1]) * 0.95;
+    object.scale.z = (treelet.bounds[1][2] - treelet.bounds[0][2]) * 0.95;
+
+    scene.add(object);
+  }
+
+  var renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  camera.position.z = 5;
+
+  var controls = new THREE.OrbitControls(camera, renderer.domElement);
+  controls.update();
+
+  var animate = function() {
+    requestAnimationFrame(animate);
+    controls.update();
+    renderer.render(scene, camera);
+  };
+
+  animate();
+}, false);
